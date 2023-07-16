d513d990f6e6fcce45677944a453c98be924348c
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-07-16 04:44:05 -0700

    Changes to be committed: 	modified:   0x01-variables_if_else_while/102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,31 +9,30 @@
 int main(void)
 {
 	int a, b;
-	
+
 	a = 0;
 
 	while (a <= 99)
 	{
-		 b = a;
-
-		 while ( b <= 99 )
-		 {
-			 if ( b != a)
-			 {
-				 putchar((a / 10) + 48);
-				 putchar((a % 10) + 48);
-				 putchar(' ');
-				 putchar((b / 10) + 48);
-				 putchar((b % 10) + 48);
-				 if ( a != 98 && b !=99)
-				 {
-					 putchar(',');
-					 putchar(' ');
-				 }
-			 }
+		b = a;
+		while (b <= 99)
+		{
+			if (b != a)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a +  b != 197)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
 			 b++;
-		 }
+		}
 		 a++;
 	}
 	putchar('\n');

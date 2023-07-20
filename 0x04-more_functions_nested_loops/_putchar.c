918922e93d5aecd55aac5f0b9bfc9f70161018f9
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-07-20 16:58:44 -0700

    Changes to be committed: 	new file:   0x04-more_functions_nested_loops/1-isdigit 	new file:   0x04-more_functions_nested_loops/1-main.c 	new file:   0x04-more_functions_nested_loops/3-main.c 	modified:   0x04-more_functions_nested_loops/_putchar.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: 1 on success
- * 		-1 on error
+ * _putchar = writes the character c to stdout
+ * description - to print out c
+ * @c: The Character to print
+ *
+ * Return: On Success 1
+ *	   On error  -1 is returned. and error no is set appropriatly
  */
-
 
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }

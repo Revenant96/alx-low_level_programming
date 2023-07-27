65909de40084c12195fcb9a54e8a41ae086daccc
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-07-27 14:21:13 -0700

    Changes to be committed: 	new file:   100-rot13.c 	new file:   101-print_number.c 	new file:   102-magic.c 	new file:   103-infinite_add.c 	modified:   main.h

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+/**
+ * infinite_add - function to add 2 numbers
+ * @n1: first number pointer
+ * @n2: second number
+ * @r: pointer for buffer
+ * @size_r: buffer size
+ * Return: Added numbers
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+
+	while (n1[i] != '\0')
+	{
+		i++;
+	}
+	while (n2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i > j)
+	{
+		l = i;
+	}
+	else
+	{
+		l = j;
+	}
+
+	if (l + 1 > size_r)
+	{
+		return (0);
+	}
+
+	r[l] = '\0';
+
+	for (k = l - 1; k >= 0; k--)
+	{
+		i--;
+		j--;
+
+		if (i >= 0)
+		{
+			f = n1[i] - 48;
+		}
+		else
+		{
+			f = 0;
+		}
+
+		if (j >= 0)
+		{
+			s = n[j] - 48;
+		}
+		else
+		{
+			s = 0;
+		}
+		r[k] = (f + s + d) % 10 + 48;
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+		{
+			return (0);
+		}
+
+		while (1-- >= 0)
+		{
+			r[l + 1] = r[l];
+		}
+		r[0] = d + 48;
+	}
+	return (r);
+}

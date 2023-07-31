4c9dca88f256e81087166df392799b71a25f7447
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-07-31 14:31:23 -0700

    Changes to be committed: 	modified:   0-memset 	modified:   0-memset.c 	new file:   0-memset1.c

--- 0x07-pointers_arrays_strings/0-memset1.c
+++ 0x07-pointers_arrays_strings/0-memset1.c
@@ -2,11 +2,12 @@
 
 /**
  * _memset - function to mimic memset
- * @s: pointer to destination string
- * @bL string to use for replacing
- * @n: Size
- * Return: updated s
+ * @s: pointer to array
+ * @b: character to add
+ * @n: size
+ * Return: updated string
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;

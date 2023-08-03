50f2a288ea43fb1a61f28716778015346bc1a2bb
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-03 12:23:13 -0700

    Changes to be committed: 	new file:   100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * is_palindrome - function to determine a palindrome
+ * @s: pointer to string to test
+ * Return: 1 if true 0 if false
+ */
+
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	for (int i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - i - 1])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}

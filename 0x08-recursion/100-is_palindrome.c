a800e1331f77fabdccc2f9b47bff02884aa70345
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-03 12:50:36 -0700

    Changes to be committed: 	modified:   100-is_palindrome.c 	new file:   100-main.c 	new file:   100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,67 @@
-#include "main.h"
-#include <string.h>
-
-/**
- * is_palindrome - function to determine a palindrome
- * @s: pointer to string to test
- * Return: 1 if true 0 if false
+/*
+ * File: 100-is_palindrome.c
+ * Auth: sami
  */
 
+#include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
 int is_palindrome(char *s)
 {
-	if (strlen(s) < 2)
-	{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
 		return (1);
-	}
-	else if(s[0] != s[strlen(s) - 1])
-	{
-		return (0);
-	}
-	else if (s[strlen(s) - 1] = '\0')
-	{
-		return (is_palindrome(&s[1]));
-	}
+
+	return (check_palindrome(s, len, index));
 }

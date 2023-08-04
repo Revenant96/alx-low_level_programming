eddb0db7de2cef30a7be6db96cd147dda592bf8d
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-04 10:22:13 -0700

    Changes to be committed: 	modified:   4-add.c 	new file:   add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,9 +21,10 @@ int main(int argc, char *argv[])
 			{
 				return(printf("Error\n"), 1);
 			}
-			sum += atoi(argv[argc]);
 		}
+		sum += atoi(argv[argc]);
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }

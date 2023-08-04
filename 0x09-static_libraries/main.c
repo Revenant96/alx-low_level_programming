f780983eb9999fd9546e7415c8d7a80b5686c6f0
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-04 09:09:23 -0700

    Changes to be committed: 	modified:   main.c 	modified:   quote

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n    - Kevin Mitnick");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t\b\b\b\b\b - Kevin Mitnick");
     return (0);
 }

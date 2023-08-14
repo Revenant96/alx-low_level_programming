59961d99ebc83f905e06348c54df35ccad2237a1
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-14 12:11:11 -0700

    Changes to be committed: 	new file:   2-main.c 	modified:   2-print_dog.c 	new file:   c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_dog - function to print struct dog
@@ -15,7 +14,8 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (*d).name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
 		if ((*d).owner == NULL)
-			;
+		{
+		}
 		else
 		{
 			printf("Owner: %s\n", (*d).owner);

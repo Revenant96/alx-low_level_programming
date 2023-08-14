96db25b26014b6804ab597c4cfdfe7263b873930
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-14 13:22:32 -0700

    Changes to be committed: 	modified:   2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
@@ -22,12 +22,11 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 		if ((*d).owner == NULL)
 		{
-			printf("Owner: (nil");
+			printf("Owner: (nil\n");
 		}
 		else
 		{
 			printf("Owner: %s\n", (*d).owner);
-		
 		}
 	}
 

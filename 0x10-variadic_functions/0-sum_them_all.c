e741dc1bcc3fd4f3aaf9ef2e81eef3ce21127852
revenant96 <Mostafa.Abdelaty2013@Gmail.com>
2023-08-17 16:01:19 -0700

    Changes to be committed: 	modified:   0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,17 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-	int s = 0;
-	int i = n;
+	int s = 0, i = n;
 	va_list ap;
 
 	if (!n)
-	{
 		return (0);
-	}
-
 	va_start(ap, n);
-
 	while (i--)
 	{
 		s += va_arg(ap, int);
